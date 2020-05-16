5d267524fdfbe47e2206828a0287fc4e83b9d167
JS Park <jsp@hashicorp.com>
2020-05-16 23:33:30 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,7 @@ variable "aws_region" {
   default     = "ap-northeast-2"
 }
 
+/*
 variable "access_key" {
   description = "AWS ACCESS KEY"
   default     = "XXXXXXXXXXXXXXXXXX"
@@ -12,7 +13,8 @@ variable "secret_key" {
   description = "AWS ACCESS SECRET KEY"
   default     = "XXXXXXXXXXXXXXXXX"
 }
-
+*/
+  
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
   default     = "ami-0ab7ae14fe6a99093"
